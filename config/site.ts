1eb5afb047bc66a92bd758057cbccb3b26e00d2f
相田 優希 <51500566+aida0710@users.noreply.github.com>
2025-03-09 00:24:25 +0900

    タイトルを修正

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-    name: 'StagRDB Web Console',
+    name: 'StegRDB Web Console',
     description: 'StegRDB Web Console is a web-based PostgreSQL client that allows you to connect to your database and run SQL queries.',
     twitter_id: '@aida_0710',
     url: 'https://www.aida0710.work',
